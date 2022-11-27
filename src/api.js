08a19dd30101e7324ccd27cf3c6ee2e8becdf503
Apple <apple@Apples-MacBook-Pro.local>
2022-11-27 19:55:28 +0530

    Added dynamic mail updates

--- src/api.js
+++ src/api.js
@@ -9,27 +9,37 @@ const app = express();
 const router = express.Router();
 router.use(bodyParser.json());
 router.use(cors());
-const transport = {
-    host: 'smtp.hostinger.com',
-    port: 465,
-    secure: true, // use TLS
-    auth: {
-        user: "info@lamhouse.in",
-        pass: "am@zecH12#",
-    },
-}
-const transporter = nodemailer.createTransport(transport)
-transporter.verify((error, success) => {
-    if (error) {
-        //if error happened code ends here
-        console.error(error)
-    } else {
-        //this means success
-        console.log('Ready to send mail!')
+
+let transporter;
+
+function SetTransportDetails(transportusername, transportpwd){
+    let transport = {
+        host: 'smtp.hostinger.com',
+        port: 465,
+        secure: true, // use TLS
+        auth: {
+            user: transportusername,
+            pass: transportpwd,
+        },
     }
-})
+    transporter = nodemailer.createTransport(transport)
+    transporter.verify((error, success) => {
+        if (error) {
+            //if error happened code ends here
+            console.error(error)
+        } else {
+            //this means success
+            console.log('Ready to send mail!')
+        }
+    })
+
+}
 
 router.post('/sendmail', (req, res, next) => {
+
+
+    SetTransportDetails(req.body.transportusername, req.body.transportpwd);
+
     //make mailable object
     const mail = {
         from: req.body.frommail,
